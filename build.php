fd10ee93d98bec7e7afab43779ff34955809495e
xaav <xaav@xaav.tk>
2011-06-13 16:16:14 -0500

    Updated builder.
    Change-Id: I695844d4ec6d4dfd1464a734d600cae144ffda28

--- build.php
+++ build.php
@@ -3,3 +3,35 @@
 /*
  * This file builds index.php
  */
+
+if(!(php_sapi_name() == 'cli' && empty($_SERVER['REMOTE_ADDR']))) {
+
+    die('This file must be run from the command line.');
+}
+
+echo "Building index.php\n";
+
+$handle = fopen(__DIR__.'/index.php', 'w+');
+
+$objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(__DIR__.'/installer'), RecursiveIteratorIterator::SELF_FIRST);
+foreach($objects as $name => $object){
+
+    echo "Adding file $name\n";
+
+    $contents = file_get_contents($name);
+
+    if(substr($contents, 0, 5) == '<?php'){
+
+        $contents = substr($contents, 5);
+    }
+
+    fwrite($handle, $contents);
+}
+
+$contents = file_get_contents(__DIR__.'/start.php');
+
+fwrite($handle, $contents);
+
+fclose($handle);
+
+echo "Successfully built index.php\n";
