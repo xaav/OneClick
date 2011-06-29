27356d717321ab906abc4ddaa77f2da8305d2714
xaav <xaav@xaav.tk>
2011-06-28 22:03:53 -0500

    Added dummy installer.
    Change-Id: I0bb80295d19555f505d2132a80ecb45009f5d415

--- src/Installer.php
+++ src/Installer.php
@@ -4,16 +4,25 @@ class Installer implements InstallerInterface
 {
     public static function getSteps()
     {
-        /**/
+        return array(
+            'firststep' => 'Downloading...',
+            'secondstep' => 'Installing...',
+        );
     }
 
     public static function processStepByName($name)
     {
-        /**/
+        switch ($name)
+        {
+            case 'firststep':
+                break;
+            case 'secondstep':
+                break;
+        }
     }
 
     public static function getPercentComplete()
     {
-        /**/
+        return .5;
     }
 }
